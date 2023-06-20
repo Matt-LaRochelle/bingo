c9dbcbac787797e0753f4f27984dcbe05bf7dd59
matthew <larochelle.matthew@gmail.com>
2023-06-20 12:15:02 -0700

    edit navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,14 +1,25 @@
 import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogout'
 import { useAuthContext } from '../hooks/useAuthContext'
+import { useState } from 'react'
 
 const Navbar = () => {
     const { logout } = useLogout()
     const { user } = useAuthContext()
+    const [nav, setNav] = useState(false);
 
     const handleClick = () => {
         logout()
     }
+
+    const openMenu = () => {
+        console.log("You clicked me!")
+        setNav(!nav)
+    }
+    const nlink = "nav-links";
+    const nactive = "nav-active";
+
+    // className={nav ? ["menu", "active"].join(' ') : ["menu"]}
 
     return (
         <header>
@@ -19,9 +30,16 @@ const Navbar = () => {
                 <nav>
                     {user && (
                         <div>
-                            <Link to="/bingo">Bingo</Link>
-                            <span>{user.email}</span>
-                            <button onClick={handleClick}>Log out</button>
+                            <div className={nav ? nactive : nlink}>
+                                <Link to="/bingo">Bingo</Link>
+                                <span>{user.email}</span>
+                                <button onClick={handleClick}>Log out</button>
+                            </div>
+                            <div className="nav-hamburger">
+                                <span onClick={openMenu} class="material-symbols-outlined">
+                                    Menu
+                                </span>
+                            </div>
                         </div>
                     )}
                     {!user && (
