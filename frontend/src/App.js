0fbc151e14a161ec99f3b9e29c7c3096fba1e3d1
matthew <larochelle.matthew@gmail.com>
2023-07-11 13:33:15 -0700

    added comment changed folder name

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,8 @@ import Signup from './pages/Signup'
 import Forgot from './pages/Forgot'
 import ResetPassword from './pages/Reset'
 
+// This is just a test for changing the folder names.
+// I want to see if it affects the way I upload things to github.
 function App() {
   const { user } = useAuthContext()
 
